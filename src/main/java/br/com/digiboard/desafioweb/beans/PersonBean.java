1816e6cf1fa4013fedbf538e07fe228c430208c8
jkmacielbr <jk.maciel.br@gmail.com>
2023-04-05 11:37:11 -0400

    implementando o cadastro na mesma tela da tabela

--- src/main/java/br/com/digiboard/desafioweb/beans/PersonBean.java
+++ src/main/java/br/com/digiboard/desafioweb/beans/PersonBean.java
@@ -5,12 +5,18 @@ import br.com.digiboard.desafioweb.model.Address;
 import br.com.digiboard.desafioweb.model.Person;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -25,17 +31,35 @@ public class PersonBean implements Serializable {
     private List<Person> personList;
     private Date now;
     private Person selectedUser;
+
     private Person person = new Person();
     private Address createAddress = new Address();
     private Address address = new Address();
-    private Person newPerson;
 
     private String searchString;
     private PersonService personService;
 
+
+    private String street;
+    private Long numberHouse;
+
+    private String neighborhood;
+
+    private String city;
+
+    private String state;
+
+    private String  country;
+
+    private String firstName;
+
+    private String lastName;
+    private LocalDate dateBirth;
+
     @Autowired
     public PersonBean(PersonService personService){
         this.personService = personService;
+        selectedUser = new Person();
 
     }
 
@@ -61,11 +85,16 @@ public class PersonBean implements Serializable {
             return filterPeople();
         }
     }
+    public void openNew(){
+        this.selectedUser = new Person();
+    }
 
     // atualizar pessoa selecionada no banco de dados
-    public void updatePerson(){
+    public String updatePerson(){
 
-        personService.update(selectedUser);
+        this.personService.update(this.selectedUser);
+
+        return "lista.app?faces-redirect=true";
     }
 
     //EXCLUSAO DE PESSOA NO BANCO DE DADOS
@@ -76,6 +105,7 @@ public class PersonBean implements Serializable {
             FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Pessoa excluída com sucesso", null);
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
+
 //        FacesContext context = FacesContext.getCurrentInstance();
 //        String url = context.getExternalContext().getRequestContextPath() + "/lista.app";
 //        try {
@@ -85,23 +115,64 @@ public class PersonBean implements Serializable {
 //        }
     }
 
+//    public String createPerson() {
+//        try {
+//            // cria uma nova pessoa com os dados informados no formulário
+//            Person newPerson = new Person();
+//            newPerson.setFirstName(person.getFirstName());
+//            newPerson.setLastName(person.getLastName());
+//            newPerson.setDateBirth(person.getDateBirth());
+//
+//            // instancia um novo objeto Address
+//            Address newAddress = new Address();
+//            newAddress.setStreet(address.getStreet());
+//            newAddress.setNumberHouse(address.getNumberHouse());
+//            newAddress.setNeighborhood(address.getNeighborhood());
+//            newAddress.setCity(address.getCity());
+//            newAddress.setState(address.getState());
+//            newAddress.setCountry(address.getCountry());
+//            newPerson.setAddress(newAddress);
+//
+//
+//            // salva a nova pessoa no banco
+//            personService.add(newPerson);
+//
+//
+//            // limpa os dados
+//            person = new Person();
+//            address = new Address();
+//
+//
+//
+//            // retorna a página lista.xhtml após salvar a nova pessoa
+//            return "index.app?faces-redirect=true";
+//
+//        } catch (Exception e) {
+//
+//            FacesContext.getCurrentInstance().addMessage(null,
+//                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao criar pessoa", null));
+//            return null;
+//        }
+//    }
 
-    public String createPerson() {
+
+    public void createPerson() {
+        Person newPerson = new Person();
+        Address newAddress = new Address();
         try {
             // cria uma nova pessoa com os dados informados no formulário
-            Person newPerson = new Person();
-            newPerson.setFirstName(person.getFirstName());
-            newPerson.setLastName(person.getLastName());
-            newPerson.setDateBirth(person.getDateBirth());
+
+            newPerson.setFirstName(this.firstName);
+            newPerson.setLastName(this.lastName);
+            newPerson.setDateBirth(this.dateBirth);
 
             // instancia um novo objeto Address
-            Address newAddress = new Address();
-            newAddress.setStreet(address.getStreet());
-            newAddress.setNumberHouse(address.getNumberHouse());
-            newAddress.setNeighborhood(address.getNeighborhood());
-            newAddress.setCity(address.getCity());
-            newAddress.setState(address.getState());
-            newAddress.setCountry(address.getCountry());
+            newAddress.setStreet(this.street);
+            newAddress.setNumberHouse(this.numberHouse);
+            newAddress.setNeighborhood(this.neighborhood);
+            newAddress.setCity(this.city);
+            newAddress.setState(this.state);
+            newAddress.setCountry(this.country);
             newPerson.setAddress(newAddress);
 
 
@@ -114,20 +185,31 @@ public class PersonBean implements Serializable {
 
 
 
+            emptyVariables();
+
             // retorna a página lista.xhtml após salvar a nova pessoa
-            return "index.app?faces-redirect=true";
+//            return "index.app?faces-redirect=true";
 
         } catch (Exception e) {
 
             FacesContext.getCurrentInstance().addMessage(null,
                     new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao criar pessoa", null));
-            return null;
+
         }
     }
+    public void emptyVariables(){
+        firstName="";
+        lastName="";
+        dateBirth=null;
+        street="";
+        numberHouse=null;
+        neighborhood="";
+        city="";
+        state="";
+        country = "";
 
 
-
-
+    }
 
 
     public String getFormattedDate(LocalDate date) {
